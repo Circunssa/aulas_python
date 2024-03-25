f6ace974f9641d44946c30fa7f545f528a85c2b3
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 15:24:33 -0300

    Concatenacao (+) e repeticao (*) com operadores aritmeticos

---
+++ aula_10.py
@@ -0,0 +1,16 @@
+print('Concatenacao (+) e repeticao (*) com operadores aritmeticos')
+
+concatenacao = 'Fabio' + ' '+ 'Eduardo'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+menu = "-" *10
+
+tres_vezes_luiz = 3 * 'Luiz,'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
+print(menu)
+print('|        |')
+print('|        |')
+print('|        |')
+print(menu)
