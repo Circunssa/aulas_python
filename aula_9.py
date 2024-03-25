c5c795606b98255307eaecacbbdb4d9bcd964ffd
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 15:24:23 -0300

    Introducao aos operadores artimeticos(matematica)

---
+++ aula_9.py
@@ -0,0 +1,22 @@
+print('Introducao aos operadores artimeticos(matematica)')
+
+adicao = 10 + 10
+print('Adicao:', adicao)
+
+sub = 10 - 5
+print('Sub:', sub)
+
+multiplicacao = 10 * 10
+print('Multiplicacao:', multiplicacao)
+
+divisao = 10 /2.2
+print('Divisao:', divisao)
+
+divisao_inteira = 10 // 2
+print('Divisao inteira', divisao_inteira)
+
+exponenciacao = 2 **10
+print('Exponenciacao:', exponenciacao)
+
+modulo = 16 % 8 # resto da divisao
+print('Modulo:', modulo)
