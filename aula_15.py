8e5d317deff7e068ab928a74c8fa99d482b486e7
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 16:53:27 -0300

    Usandoa a funcao input para coletar dados do usuario

---
+++ aula_15.py
@@ -0,0 +1,5 @@
+print('Usandoa a funcao input para coletar dados do usuario')
+name = input('Qual o seu naome? ')
+idade =input('Digite a sua idade')
+idade_coersao = int(idade) 
+print(f'Seu nome e {name=}e a sua idade: {idade_coersao=}')
