e329e159a6ac54ba467209e242849588ed9d231a
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 16:53:47 -0300

    if, elif and else : entendendo fluxo

---
+++ aula_17.py
@@ -0,0 +1,25 @@
+print('if, elif and else : entendendo fluxo')
+
+
+condition_1 = False
+condition_2 = False
+condition_3 = True
+condition_4 = False
+
+
+if condition_1:
+    print('condition 1')
+elif condition_2:
+    print('condition 2')
+elif condition_3:
+    print('condition 3')
+elif condition_4:
+    print('condition 4')
+else:
+    print('nenhuma condicao foi satisfeit')
+
+if 10==10:
+    print('Este e o codigo outro if')
+else:
+    print('else outro else')
+print('fora do if')
