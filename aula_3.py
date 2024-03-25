0f2a9092951827081b6dd072d844e6d18b5ed5b4
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 15:23:32 -0300

    Python = Linguagem de programacao     tipo de tipagem  = dinamica / forte

---
+++ aula_3.py
@@ -0,0 +1,20 @@
+"""
+    Python = Linguagem de programacao 
+    tipo de tipagem  = dinamica / forte
+
+    str -> string -> texto
+    Strings sao textos que estao dentro de aspas
+"""
+
+#aspas simples
+print('aaa')
+#aspas duplas
+print("aaa")
+#escape
+print("aaa \"bbb")
+
+#r
+print(r"aaaaa \"bbb\"")
+
+#inverte as aspas simples para poder usar o dupla e assim vice e versa
+print('aaaa "bbbb" ccccc')
