c2af753f5fff1f8a3bf5915f0c807122a2b39752
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 15:24:50 -0300

    Introducao as f-strings(formatacao de strings)

---
+++ aula_13.py
@@ -0,0 +1,9 @@
+print('Introducao as f-strings(formatacao de strings)')
+
+nome = 'Fabio Eduardo'
+altura = 1.73
+peso = 105
+imc = int(peso/int(altura **2 ))
+
+print(f'Nome:, {nome} , altura:, {altura:.2f}, peso:, {peso} , seu imc:, {imc}')
+
