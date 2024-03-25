002368700349030881292a1592a4432398e557f0
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 15:25:17 -0300

    exercicios

---
+++ exercicios/aula_8_exercicios.py
@@ -0,0 +1,15 @@
+nome = 'Fabio Eduardo '
+sobrenome = 'Circuncisao'
+idade = 30
+ano_nascimento = 2023 - idade
+idade_maior = 18
+maior_de_idade =  idade >=idade_maior
+altura_centimentro = 175
+altura_metros = 0.01
+convert_cm_metros =altura_metros * altura_centimentro
+print('Name:',nome)
+print('Sobrenome:',sobrenome)
+print('Idade:',idade)
+print('Ano de nascimento:',ano_nascimento)
+print('E maior de idade:',maior_de_idade)
+print('Altura em metros:',convert_cm_metros,'metros')
