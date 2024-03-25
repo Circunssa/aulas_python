c1d1247e45eb5d680dec375ac75f5d636b1bc782
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 15:24:13 -0300

    Introducao as variaveis em python

---
+++ aula_7.py
@@ -0,0 +1,11 @@
+print('Introducao as variaveis em python')
+
+"""
+Variaveis sao usadas para salvar algo na memoria do computador.
+PEP 8: inicie variaveis ocm letras minusculas, pode usar numeros e underline _.
+O sinal de = e operador de atribuicao. Ele e usado apra atribuir um valor a um nome (variavel).
+Uso: nome_variavel = expressao.
+"""
+nome_completo = 'Fabio Eduardao Circuncisao'
+idade=30
+print('Nome:',nome_completo, 'Idade:', idade)
