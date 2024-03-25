c80c694eb6e648953d3e35b6083acfd2e6d5f186
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 16:53:36 -0300

    if/ elif  else

---
+++ aula_16.py
@@ -0,0 +1,13 @@
+print('if/ elif  else')
+
+entrada = input(' Voce quer "sair" ou "ficar"?')
+out = 'sair'
+no_out = 'ficar'
+
+
+if entrada == out:
+    print('bye bye')
+elif entrada == no_out:
+    print('ok ok ')
+else:
+    print('i dont fuck care')
