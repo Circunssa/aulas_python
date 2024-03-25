bbede5e4c754ce33094199b4d7e3f1263665a203
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-25 15:24:05 -0300

    Coesao de tipos (convertendo um tipo para outro)

---
+++ aula_6.py
@@ -0,0 +1,19 @@
+print('Coesao de tipos (convertendo um tipo para outro)')
+"""
+conversao de tipo, coercao
+tupe convertion, typecasting, coercion
+e o ato de converter um tipo em outro 
+tipos imutaveis e primitivos:
+str, int, float, bool
+"""
+convert_one = int('1')
+convert_bool = bool('')
+convert_str = str()
+convert_s = 's'
+#verifica o tipo 
+type()
+
+print (1+1)
+print (type(convert_one + convert_one))
+print (type(convert_s))
+print ('s'+'a') 
