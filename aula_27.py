bfb798230a54a436c20a9f989ad42ad8b7cbfd9b
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-27 10:46:15 -0300

    Fatiamento de strings

---
+++ aula_27.py
@@ -0,0 +1,11 @@
+"""
+Fatiamento de strings 
+012345678
+Ola mundo
+-987654321
+
+Fatiamento [i:f:p] [::]
+Obs: a funcao len retorna a qtd de carateres da str
+"""
+variavel = 'Ola mundo'
+print(variavel[0:9:2])
