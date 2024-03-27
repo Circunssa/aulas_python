50931524e2140d784d331f45db72fddf7fd0c54f
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-27 10:45:26 -0300

    Operadores logicos AND(e)

---
+++ aula_21.py
@@ -0,0 +1,22 @@
+print('Operadores logicos AND(e)')
+
+"""
+And (e) or (or) not (nao)
+and - todas as condicoes precisam ser verdadeiras.
+Se qualquer valor for considerado falso,
+a expressao inteira sera avaliada naquele valor
+Sao considerados Falsy (que voce ja viu ) 0, 0.0 , '' False
+Tambem existe o tipo None que usado para representar um valor
+
+"""
+
+entrada = input('[E]ntrar [S]air:')
+password = input('Pass:')
+
+check_password = '123456'
+if entrada == 'E' and check_password == password:
+    print('Entrar')
+else:
+    print('Sair')
+
+
