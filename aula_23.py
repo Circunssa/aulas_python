ac90539f070c0ed92c67291df40c573d6ea90a21
Caffrankseria <circuncisaofabio@icloud.com>
2024-03-27 10:45:43 -0300

    Operadores logicos Not(nao)

---
+++ aula_23.py
@@ -0,0 +1,18 @@
+print('Operadores logicos Not(nao)')
+
+"""
+And (e) or (or) not (nao)
+and - todas as condicoes precisam ser verdadeiras.
+Se qualquer valor for considerado falso,
+a expressao inteira sera avaliada naquele valor
+Sao considerados Falsy (que voce ja viu ) 0, 0.0 , '' False
+Tambem existe o tipo None que usado para representar um valor
+
+
+not True = False
+not False = True
+"""
+print(not True)
+print(not False)
+
+
